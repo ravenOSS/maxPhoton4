c2865fd86df274ce0f39f1a4f936c1d2d0d22bc2
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-05-24 14:48:09 -0600

    Copy code to .ino

--- src/maxPhoton4.ino
+++ src/maxPhoton4.ino
@@ -1,18 +1,81 @@
-/*
+ /*
  * Project maxPhoton4
- * Description:
- * Author:
- * Date:
+ * Description: Interface Maxbotix serial sensor to Particle Photon or Arduino compatible
+ * Author: David Richards / ravenIoT LLC
+ * Date: May 22, 2019
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+// Using hardware UART on Photon
+// Maxbotix sensor with RS232 conversion to TTL with MAX3232
+// Name mySerial => Serial1 (Photon definition)
 
+char inChar; // type for data read
+char buf[5]; // array to store range data
+int range = 0;   // type declaration
+char inByte;  // not used
+
+void setup()
+{
+  Serial.begin(57600);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect.
+  }
+  Serial.println("Serial open");
+  // set the data rate from the sensor
+  Serial1.begin(9600, SERIAL_8N1); // Default but set explicitly anyway
+  while (!Serial1)
+  {
+    ; // wait for Serial1 port to connect.
+  }
+  Serial.println("Sensor connected");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
-
+void loop()
+{
+  range = maxRead();
+  Serial.print("Range: ");
+  Serial.println(range);
+  delay(5000);
 }
+
+int maxRead()
+{
+  // Serial1.flush();
+  if (Serial1.available() != 0){
+    Serial1.read();
+  }
+  while (Serial1.available() > 0)
+  {
+    inChar = Serial1.read();
+    if (inChar == 82)
+    {
+       Serial.println("Got 82");
+      range = Serial1.parseInt();
+      // Serial.println(range);
+    }
+  }
+  return range;
+}
+
+// int maxRead()
+// {
+//   while (Serial1.available() > 0)
+//   {
+//     inChar = Serial1.read();  // continuously read sensor input in while loop
+//     if (inChar == 'R')        // test if char == R for beginning data
+//     {
+//       for (int i; i < 4; i++) // capture the next three characters
+//       {
+//         inChar = Serial1.read();
+//         buf[i] = inChar; // assign input char to buf index
+//         Serial.print("char =");
+//         Serial.println(inChar);
+//       }
+//     }
+//     Serial.print("buf =");
+//     Serial.println(buf);
+//     range = parseInt(buf); // extract integer range from char buf
+//   }
+//   return range;
+// }
